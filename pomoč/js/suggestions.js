dce3fcb44e7c37404df286bf8f1eb10a5a8a3201
RealSlovenianGamer <87998044+RealSlovenianGamer@users.noreply.github.com>
2021-08-30 13:35:05 +0200

    Add files via upload
    pomoč v1.0

---
+++ pomoč/js/suggestions.js
@@ -0,0 +1,6 @@
+let suggestions = [
+    "Kako se lahko pridružim Discord serverju",
+    "Kako se preverim v Discord serverju",
+    "Kako se lahko sam preverim brez pomoči osebja",
+    "Kaj pomeni če osebje napiše poizvedbo mojega Minecraft imena in se pokaže error",
+];
